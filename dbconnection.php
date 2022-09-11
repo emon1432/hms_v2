ad9bc02fa3ef94207506fc1a81f72fb424e3fd87
emon1432 <ovirajemon143@gmail.com>
2022-09-11 23:46:51 +0600

    update some text

--- dbconnection.php
+++ dbconnection.php
@@ -1,3 +1,3 @@
 <?php
 // Create connection
-$con = mysqli_connect("localhost", "root", "", "hms2") or die("Connection Failed : " . mysqli_connect_errno());
+$con = mysqli_connect("localhost", "root", "", "hms_v2") or die("Connection Failed : " . mysqli_connect_errno());
